4d6e6280a8cd514660b7ec14430cc2235471d810
Kornel Zydorczyk <zydorczyk.kornel@gmail.com>
2023-03-06 21:05:04 +0100

    css update

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
+let input = document.getElementById("io");
 function IoClear() {
-    let input = document.getElementById("io");
     input.value = "";
 }
 
 function Eingabe(value){
-    let input = document.getElementById("io");
     input.value += value;
     console.log(value);
 }
 
 function Rechnen(){
-    let input = document.getElementById("io");
     input.value = eval(input.value);
 }
